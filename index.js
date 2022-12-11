4812bbfb0c1592bda5150e8e44aa735c92e5d621
aim <ahmadhakim404@gmail.com>
2022-12-11 15:54:18 +0800

    setup frontend

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/openai', require('./routes/openaiRoutes'));
 
 app.listen(port, () => console.log(`server started on port: ${port}`));
